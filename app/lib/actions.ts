83fca4f8f3f6f15198f02595436dc7dbbaf0b6b6
Felipe Mahlmeister <felipe.mahlmeister1@gmail.com>
2024-01-22 10:37:08 -0300

    Validate and prepare the data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,34 @@
 'use server';
 
+import { z } from 'zod';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+  });
+
+  const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+
 export async function createInvoice(formData: FormData) {
     // const rawFormData = Object.fromEntries(formData.entries())
 
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
-    };
+    });
+
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    
     // Test it out:
-    console.log(rawFormData);
+    console.log(customerId, amount, status, date);
+    console.log(typeof customerId);
+    console.log(typeof amount);
+    console.log(typeof status);
+    console.log(typeof date);
   }
