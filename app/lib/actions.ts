aac3defe2ba5129c80e8c30fac234c793b62172f
Felipe Mahlmeister <felipe.mahlmeister1@gmail.com>
2024-01-22 10:28:38 -0300

    Extract the data from formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,13 @@
 'use server';
 
-export async function createInvoice(formData: FormData) {}
+export async function createInvoice(formData: FormData) {
+    // const rawFormData = Object.fromEntries(formData.entries())
+
+    const rawFormData = {
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    };
+    // Test it out:
+    console.log(rawFormData);
+  }
